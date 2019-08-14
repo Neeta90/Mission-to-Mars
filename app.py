332d6591e4ca7598cd6d81d2fc4d9677467558cd
Neeta90 <Neetashrivastava90.gmail,com>
2019-08-13 23:27:03 -0500

    final commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, redirect
+import pymongo
+import scrape_mars
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+#app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
+#mongo = pymongo(app)
+
+# Or set inline
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
+
+# setup mongo connection
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+db=client.mars1_db
+collection=db.mars_info
+#mars1_db.mars_data.insert_many(mars_info)
+#db.collection.insert_many(mars_data)
+
+
+@app.route("/")
+def home():
+    mars_data= db.mars_facts.find_one()
+    #mars_data=list(db.collection.find())
+    return render_template("index.html", mars_data=mars_data)
+
+
+
+@app.route("/scrape")
+def scrape():
+    mars_data = db.mars_facts
+    mars_fact= scrape_mars.scrape_mars_news()
+    mars_fact = scrape_mars.scrape_mars_image()
+    mars_fact = scrape_mars.scrape_mars_weather()
+    mars_fact = scrape_mars.scrape_mars_facts()
+    mars_fact = scrape_mars.scrape_mars_hemispheres()
+    mars_data.update({}, mars_fact, upsert=True)
+# redirect the route 
+    return redirect("/", code=302)
+
+# Port number is optional
+if  __name__ == "__main__":
+    app.run(port=5002,debug=True)
